79e66aed02fd8231df0532602e47c20e0ea368fb
Roman Vey <roman.vey@gmail.com>
2017-07-19 20:14:24 +0300

    Added .gitignore and replaced hw6 to ES2015 standart

---
+++ homework6/src/js/userList.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+
+export function userList(users){
+    const container = document.getElementById('root');
+    const sortedUsers = _.sortBy(users, 'age');
+    this.showList = () => {
+        sortedUsers.forEach((user) => {
+            const div = document.createElement("div");
+            div.append(user.name + ' ' + user.age);
+            container.appendChild(div);
+        });
+    }
+}
+
