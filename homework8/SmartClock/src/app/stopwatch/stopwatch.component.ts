22bae85ecd914bbf2169f3a025b2e007ace1ca91
Roman Vey <roman.vey@gmail.com>
2017-07-27 02:12:04 +0300

    stopwatch basics

--- homework8/SmartClock/src/app/stopwatch/stopwatch.component.ts
+++ homework8/SmartClock/src/app/stopwatch/stopwatch.component.ts
@@ -9,9 +9,67 @@ import {StopwatchService} from './../services/stopwatch.service'
 })
 export class StopwatchComponent implements OnInit {
 
-  constructor(public stopwatchService: StopwatchService) { }
+  time:number;
+  lapTime: number;
+  intervalId: any;
+  timeStr: string;
+  started: boolean;
+  lapTimeStr: string;
+  laps: number[];
+  avg: number;
+  
+  constructor(public stopWatchService: StopwatchService) { }
 
   ngOnInit() {
+    this.laps = [];
+    this.time = 0;
+    this.lapTime = 0;
+    this.avg = 0;
+    this.started = false;
+    this.timeStr = this.stopWatchService.convertTimeToStr(this.time);   
+  }
+  
+  startStopwatch(){
+    console.log("Stopwatch: startStopwatch()");
+
+    this.started = true;
+    this.intervalId = setInterval(()=>{
+     this.time += 100;
+     this.lapTime += 100;
+     this.timeStr = this.stopWatchService.convertTimeToStr(this.time);
+     this.lapTimeStr = this.stopWatchService.convertTimeToStr(this.lapTime);
+    },100)
+    
+  }
+  
+  pauseStopwatch(){
+    console.log("Timer: pauseTimer()");
+    this.timeStr = this.stopWatchService.convertTimeToStr(this.time);
+    this.lapTimeStr = this.stopWatchService.convertTimeToStr(this.lapTime);
+    this.started = false;
+    clearInterval(this.intervalId);
+  }
+
+
+  clearStopwatch(){
+    console.log("Timer: clearTimer()");
+    this.time = 0;
+    this.lapTime = 0;
+    this.laps = [];
+    this.pauseStopwatch();
+    this.stopWatchService.clearLaps();
+  }
+
+  lap(){
+    console.log(this.lapTime);
+    console.log(this.lapTimeStr);
+    this.lapTimeStr = this.stopWatchService.convertTimeToStr(this.lapTime);
+    console.log(this.lapTime);
+    this.laps.push(this.lapTime);
+    console.log(this.laps);
+    this.lapTime = 0;
+    this.avg = this.stopWatchService.getAvg(this.laps);
+    this.stopWatchService.updateLaps(this.avg, this.laps);
   }
 
 }
